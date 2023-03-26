8e84b5a162cfad708d506fd09b0915a205f83e70
ddaniel100002 <drc853083@gmail.com>
2023-03-26 17:48:06 +0300

    Fixed Height, but no satisfied. end of day 4

--- eshop/frontend/src/pages/HomePage.js
+++ eshop/frontend/src/pages/HomePage.js
@@ -51,7 +51,7 @@ function HomePage() {
         ) : (
           <Row>
             {products.map((product) => (
-              <Col key={product.token} lg={3} md={4} sm={6} className="mb-3 products">
+              <Col key={product.token} lg={3} md={4} sm={6} className="mb-3">
                 <Product product={product}></Product>
               </Col>
             ))}
