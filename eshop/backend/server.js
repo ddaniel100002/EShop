43608b77c1dc31f70df4359822a510ce15fb2ba9
ddaniel100002 <drc853083@gmail.com>
2023-04-01 22:51:31 +0300

    End of day 5, i think now its fixed

--- eshop/backend/server.js
+++ eshop/backend/server.js
@@ -6,7 +6,7 @@ import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoutes.js';
 
 dotenv.config();
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
